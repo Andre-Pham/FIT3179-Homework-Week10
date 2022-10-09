28819f60e0303ca6a3612c60e62fbb433330aab9
Andre Pham <andrepham33@gmail.com>
2022-10-09 18:28:09 +1100

    Cleaning up

---
+++ js/setup.js
@@ -0,0 +1,3 @@
+var areachart = "js/areachart.json";
+vegaEmbed('#areachart', areachart).then(function(result) { })
+    .catch(console.error);
--- js/vega_lite_linechart.js
+++
@@ -1,3 +0,0 @@
-var linechart = "js/us_aqi_linechart.json";
-vegaEmbed('#linechart', linechart).then(function(result) { })
-    .catch(console.error);
